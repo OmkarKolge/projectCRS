dd656efd023717a6983e6a042e9a1a77a4f49c0c
OmkarKolge <omkarkolgem@gmail.com>
2022-04-12 22:09:32 +0530

    CarRentalSystemFrontend

---
+++ FrontEnd/car-rental/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6>© 2021 - 2022 All Rights Reserved</h6>
+    </div>
+  )
+}
+
+export default Footer
